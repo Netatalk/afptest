4df6e424f67cb9fed92a899454675c582374832a
didg <didg>
2004-01-30 16:25:27 +0000

    test for network trash handling

--- test/FPByteRangeLock.c
+++ test/FPByteRangeLock.c
@@ -2,6 +2,8 @@
 */
 #include "specs.h"
 
+static char temp[MAXPATHLEN];   
+
 /* -------------------------- */
 STATIC void test60()
 {
@@ -397,9 +399,6 @@ u_int16_t bitmap = 0;
 u_int16_t vol2;
 DSI *dsi2;
 int type = OPENFORK_DATA;
-#if 0
-int len = (type == OPENFORK_RSCS)?(1<<FILPBIT_RFLEN):(1<<FILPBIT_DFLEN);
-#endif
 
     fprintf(stderr,"===================\n");
     fprintf(stderr,"FPByteRangeLock:test329: FPByteLock 2users DATA FORK\n");
@@ -440,11 +439,6 @@ int len = (type == OPENFORK_RSCS)?(1<<FILPBIT_RFLEN):(1<<FILPBIT_DFLEN);
 	
 	FAIL (FPCloseFork(Conn,fork2))
 
-#if 0
-	FAIL (FPSetForkParam(Conn, fork, len , 50))
-	FAIL (FPByteLock(Conn, fork, 0, 0 , 0 , 100))
-#endif
-
 	dsi2 = &Conn2->dsi;
 	vol2  = FPOpenVol(Conn2, Vol);
 	if (vol2 == 0xffff) {
@@ -473,24 +467,6 @@ int len = (type == OPENFORK_RSCS)?(1<<FILPBIT_RFLEN):(1<<FILPBIT_DFLEN);
 		failed();
 	}
 	
-#if 0
-	if (!fork1) {
-		failed();
-	}
-	else {
-		FAIL (htonl(AFPERR_LOCK) != FPRead(Conn2, fork1, 0, 40, Data)) 
-		FAIL (htonl(AFPERR_LOCK) != FPWrite(Conn2, fork1, 10, 40, Data, 0))
-		FAIL (htonl(AFPERR_LOCK) != FPWrite(Conn2, fork1, 55, 40, Data, 0))
-		FAIL (FPSetForkParam(Conn2, fork1, len , 60)) 
-		FAIL (FPWrite(Conn, fork, 55, 40, Data, 0)) 
-		FAIL (htonl(AFPERR_LOCK) != FPSetForkParam(Conn2, fork1, len , 60)) 
-		FAIL (htonl(AFPERR_LOCK) != FPByteLock(Conn2, fork1, 0, 0 /* set */ , 20, 60))
-		FAIL (FPSetForkParam(Conn, fork, len , 200))
-		FAIL (FPSetForkParam(Conn2, fork1, len ,120))
-		
-	}
-#endif
-	
 	FAIL (FPCloseFork(Conn,fork))
 	if (fork1) FPCloseFork(Conn,fork1);
 	if (fork2) FPCloseFork(Conn2,fork2);
@@ -500,6 +476,256 @@ fin:
 	FAIL (FPDelete(Conn, vol,  DIRDID_ROOT, name))
 }
 
+/* ------------------- */
+static int create_trash(CONN *conn, u_int16_t vol)
+{
+char *trash = "Network Trash Folder";
+int  ofs =  3 * sizeof( u_int16_t );
+u_int16_t bitmap = (DIRPBIT_ATTR)| (1<<DIRPBIT_FINFO)| (1<<DIRPBIT_CDATE) | 
+					(1<<DIRPBIT_BDATE) | (1<<DIRPBIT_MDATE)| (1<<DIRPBIT_UID) |
+	    			(1 << DIRPBIT_GID) |(1 << DIRPBIT_ACCESS);
+DSI *dsi;
+int dir;
+struct afp_filedir_parms filedir;
+
+	dsi = &conn->dsi;
+
+	dir  = FPCreateDir(conn, vol, DIRDID_ROOT, trash);
+	if (!dir) {
+		return dir;
+	}
+
+	if (FPGetFileDirParams(conn, vol,  DIRDID_ROOT , trash, 0,bitmap )) {
+		FPDelete(Conn, vol,  dir, "");
+		return 0;
+	}
+	filedir.isdir = 1;
+
+	afp_filedir_unpack(&filedir, dsi->data +ofs, 0, bitmap);
+    filedir.access[0] = 0; 
+    filedir.access[1] = 7; 
+    filedir.access[2] = 7; 
+    filedir.access[3] = 7; 
+    if (FPSetDirParms(conn, vol, dir , "", (1 << DIRPBIT_ACCESS),  &filedir)) {
+		FPDelete(conn, vol,  dir, "");
+		return 0;
+    }
+
+	filedir.attr = ATTRBIT_INVISIBLE| ATTRBIT_SETCLR ;
+    if (FPSetDirParms(conn, vol, DIRDID_ROOT , trash, (1 << DIRPBIT_ATTR), &filedir)) {
+		FPDelete(conn, vol,  dir, "");
+		return 0;
+    }
+	return dir;
+}
+
+/* ------------------- */
+static int create_map(CONN *conn, u_int16_t vol, int dir, char *name)
+{
+DSI *dsi;
+struct afp_filedir_parms filedir;
+int  ofs =  3 * sizeof( u_int16_t );
+int fork;
+
+	dsi = &conn->dsi;
+
+	if (FPCreateFile(conn, vol,  0, dir , name)) {
+		return 0;
+	}
+
+	if (FPGetFileDirParams(conn, vol,  dir , name, 0x73f, 0x133f )) {
+	  	return 0;
+	}
+	filedir.isdir = 0;
+	afp_filedir_unpack(&filedir, dsi->data +ofs, 0x73f, 0);
+	filedir.attr = ATTRBIT_INVISIBLE| ATTRBIT_SETCLR ;
+ 	if (FPSetFilDirParam(conn, vol, dir , name, (1 << DIRPBIT_ATTR), &filedir)) {
+	  	return 0;
+ 	}
+
+	fork = FPOpenFork(conn, vol, OPENFORK_DATA , 0x342 , dir, name, OPENACC_DWR | OPENACC_RD);
+	return fork;
+}
+
+/* ------------------- */
+static int set_perm(CONN *conn, u_int16_t vol, int dir)
+{
+int  ofs =  3 * sizeof( u_int16_t );
+u_int16_t bitmap = (DIRPBIT_ATTR)| (1<<DIRPBIT_FINFO)| (1<<DIRPBIT_CDATE) | 
+					(1<<DIRPBIT_BDATE) | (1<<DIRPBIT_MDATE)| (1<<DIRPBIT_UID) |
+	    			(1 << DIRPBIT_GID) |(1 << DIRPBIT_ACCESS);
+DSI *dsi;
+struct afp_filedir_parms filedir;
+
+	dsi = &conn->dsi;
+
+	if (FPGetFileDirParams(conn, vol,  dir , "", 0,bitmap )) {
+		return 0;
+	}
+	filedir.isdir = 1;
+
+	afp_filedir_unpack(&filedir, dsi->data +ofs, 0, bitmap);
+    filedir.access[0] = 0; 
+    filedir.access[1] = 0; 
+    filedir.access[2] = 0; 
+    filedir.access[3] = 7; 
+    if (FPSetDirParms(conn, vol, dir , "", (1 << DIRPBIT_ACCESS),  &filedir)) {
+		return 0;
+    }
+
+	filedir.attr = ATTRBIT_INVISIBLE| ATTRBIT_SETCLR ;
+    if (FPSetDirParms(conn, vol, dir , "", (1 << DIRPBIT_ATTR), &filedir)) {
+		return 0;
+    }
+    return 1;
+}
+
+/* ------------------- */
+static int write_access(CONN *conn, u_int16_t  vol, int dir)
+{
+int  ofs =  3 * sizeof( u_int16_t );
+u_int16_t bitmap = (DIRPBIT_ATTR)| (1<<DIRPBIT_FINFO)| (1<<DIRPBIT_CDATE) | 
+					(1<<DIRPBIT_BDATE) | (1<<DIRPBIT_MDATE)| (1<<DIRPBIT_UID) |
+	    			(1 << DIRPBIT_GID) |(1 << DIRPBIT_ACCESS);
+DSI *dsi;
+struct afp_filedir_parms filedir;
+
+	dsi = &conn->dsi;
+
+	if (FPGetFileDirParams(conn, vol,  dir , "", 0,bitmap )) {
+		return 0;
+	}
+	filedir.isdir = 1;
+	afp_filedir_unpack(&filedir, dsi->data +ofs, 0, bitmap);
+	if ((filedir.access[0] & 7) == 7)
+		return 1;
+	return 0;
+}
+
+/* --------------- */
+static int init_trash(CONN *conn, u_int16_t vol, int *result)
+{
+char *trash = "Network Trash Folder";
+char *map = "Trash Can Usage Map";
+int ret;
+int dir;
+int dir2;
+int fork = 0;
+DSI *dsi;
+int indice = 1;
+
+	dsi = &conn->dsi;
+
+	/* ---------- check/create 'Network Trash Folder' -------- */
+	ret = FPGetFileDirParams(conn, vol,  DIRDID_ROOT , trash, 0x73f, 0x133f);
+	if (ret == ntohl(AFPERR_NOOBJ)) {
+	    dir = create_trash(conn, vol);
+	    
+	    if (!dir) {
+		    nottested();
+		    return 0;
+	    }
+	}
+	else if (ret) {
+	    nottested();
+	    return 0;
+	}
+	dir = get_did(conn, vol, DIRDID_ROOT , trash);
+	if (!dir) {
+	    nottested();
+	    return 0;
+	}
+
+	/* --------- check/create 'Trash Can Usage Map' -------- */
+	fork = FPOpenFork(conn, vol, OPENFORK_DATA , 0x342 , dir, map, OPENACC_DWR |OPENACC_RD);
+	if (!fork && ntohl(AFPERR_NOOBJ) != dsi->header.dsi_code) {
+	    nottested();
+	    return 0;
+	}
+	if (!fork) {
+		fork = create_map(conn, vol, dir, map);
+	}
+	if (!fork) {
+	    nottested();
+	    return 0;
+	}
+
+	if (FPGetForkParam(conn, fork, 0x242)) {
+		nottested();
+		return 0;
+	}
+
+	/* */
+	while (1) {
+		indice++;
+		if (FPByteLock(conn, fork, 0, 0 /* set */, indice , 1))
+			continue;
+		sprintf(temp, "Trash Can #%d", indice);
+		dir2 = FPCreateDir(conn, vol, dir, temp);
+		if (dir2) {
+		   if (!set_perm(conn, vol, dir2)) {
+		       nottested();
+		       return 0;
+		   }
+		   break;
+		
+		}
+		else {
+		    dir2 = get_did(conn, vol, dir , temp);
+		    if (dir2 && write_access(conn, vol, dir2)) {
+		        break;
+		    }
+		}
+		
+		if (FPByteLock(conn, fork, 0, 1 /* clear */, indice , 1)) {
+			nottested();
+			return 0;
+		}
+	}
+	*result = dir2;
+	return fork;
+}
+
+/* --------------- */
+STATIC void test330()
+{
+u_int16_t vol = VolID;
+u_int16_t vol2;
+int fork = 0;
+int fork2 = 0;
+DSI *dsi2;
+int dir, dir2;
+
+	if (!Conn2) {
+		test_skipped(T_CONN2);
+		return;
+	}
+	fork = init_trash(Conn, vol, &dir);
+	if (!fork)
+	    goto fin;
+	
+	dsi2 = &Conn2->dsi;
+	vol2  = FPOpenVol(Conn2, Vol);
+	if (vol2 == 0xffff) {
+		nottested();
+		goto fin;
+	}
+	fork2 = init_trash(Conn2, vol2, &dir2);
+	if (!fork2)
+		goto fin;
+	if (dir2 == dir) {
+		fprintf(stderr,"\tFAILED both client are using the same folder for trash\n"); 
+		failed_nomsg();
+	}
+
+	if (fork2) FPCloseFork(Conn2, fork2);
+	FAIL (FPCloseVol(Conn2,vol2))
+
+fin:
+	if (fork) {
+		FPCloseFork(Conn, fork);
+	}
+}
 
 /* ----------- */
 void FPByteRangeLock_test()
@@ -514,5 +740,6 @@ void FPByteRangeLock_test()
 	test79();
 	test80();
 	test329();
+	test330();
 }
 
