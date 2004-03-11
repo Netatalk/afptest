7563b2ee40d0ba7cea7e014f1120eac7a2f86692
didg <didg>
2004-03-11 01:42:14 +0000

    more tests, test108 within the same folder.

--- test/FPExchangeFiles.c
+++ test/FPExchangeFiles.c
@@ -36,16 +36,12 @@ u_int16_t vol = VolID;
 
 	/* test remove of no cnid db */
 	if ((temp = get_fid(Conn, vol, DIRDID_ROOT , name)) != fid_name) {
-		fprintf(stderr,"\tIGNORED %x should be %x\n", temp, fid_name);
-#if 0
+		fprintf(stderr,"\tFAILED %x should be %x\n", temp, fid_name);
 		failed_nomsg();
-#endif		
 	}
 	if ((temp = get_fid(Conn, vol, dir , name1)) != fid_name1) {
-		fprintf(stderr,"\tIGNORED %x should be %x\n", temp, fid_name1);
-#if 0
+		fprintf(stderr,"\tFAILED %x should be %x\n", temp, fid_name1);
 		failed_nomsg();
-#endif
 	}
 
 	read_fork(Conn, vol,  DIRDID_ROOT , name, 3);
@@ -139,17 +135,13 @@ int ret;
 	
 	if (fork) FPCloseFork(Conn,fork);
 	if ((ret = get_fid(Conn, vol, DIRDID_ROOT , name)) != fid_name) {
-		fprintf(stderr,"\tIGNORED %x should be %x\n", ret, fid_name);
-#if 0
+		fprintf(stderr,"\tFAILED %x should be %x\n", ret, fid_name);
 		failed_nomsg();
-#endif		
 	}
 
 	if ((ret = get_fid(Conn, vol, dir , name1)) != fid_name1) {
-		fprintf(stderr,"\tIGNORED %x should be %x\n", ret, fid_name1);
-#if 0
+		fprintf(stderr,"\tFAILED %x should be %x\n", ret, fid_name1);
 		failed_nomsg();
-#endif		
 	}
 		
 fin:
@@ -200,6 +192,62 @@ u_int16_t vol = VolID;
 	FAIL (FPDelete(Conn, vol,  DIRDID_ROOT, ndir))
 }
 
+/* ------------------------- */
+STATIC void test342()
+{
+int dir;
+char *name  = "t342 exchange file";
+char *name1 = "t342 new file name";
+int fid_name;
+int fid_name1;
+int temp;
+u_int16_t vol = VolID;
+
+    fprintf(stderr,"===================\n");
+    fprintf(stderr,"FPExchangeFiles:test108: exchange files\n");
+
+	if (FPCreateFile(Conn, vol,  0, DIRDID_ROOT , name)) {
+		nottested();
+		return;
+	}
+	dir = DIRDID_ROOT;
+	FAIL (FPCreateFile(Conn, vol,  0, dir, name1))
+
+	fid_name  = get_fid(Conn, vol, DIRDID_ROOT , name);
+
+	fid_name1 = get_fid(Conn, vol, dir , name1);
+
+	write_fork( Conn, vol, DIRDID_ROOT , name, "blue");
+	write_fork( Conn, vol, dir , name1, "red");
+	/* ok */
+	FAIL (FPExchangeFile(Conn, vol, DIRDID_ROOT, dir, name, name1)) 
+
+	/* test remove of no cnid db */
+	if ((temp = get_fid(Conn, vol, DIRDID_ROOT , name)) != fid_name) {
+		fprintf(stderr,"\tFAILED %x should be %x\n", temp, fid_name);
+		failed_nomsg();
+	}
+	if ((temp = get_fid(Conn, vol, dir , name1)) != fid_name1) {
+		fprintf(stderr,"\tFAILED %x should be %x\n", temp, fid_name1);
+		failed_nomsg();
+	}
+
+	read_fork(Conn, vol,  DIRDID_ROOT , name, 3);
+	if (strcmp(Data,"red")) {
+		fprintf(stderr,"\tFAILED should be red\n");
+		failed_nomsg();
+	}
+	read_fork(Conn,  vol, dir , name1, 4);
+	if (strcmp(Data,"blue")) {
+		fprintf(stderr,"\tFAILED should be blue\n");
+		failed_nomsg();
+	}
+	FAIL (FPDelete(Conn, vol,  dir , name1))
+
+	FAIL (FPDelete(Conn, vol,  DIRDID_ROOT, name))
+}
+
+
 /* ----------- */
 void FPExchangeFiles_test()
 {
@@ -208,5 +256,6 @@ void FPExchangeFiles_test()
 	test108();
 	test111();
 	test197();
+	test342();
 }
 
