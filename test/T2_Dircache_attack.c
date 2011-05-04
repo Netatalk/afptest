814879582caa844fe8956f785d69fba78c554d1e
Frank Lahm <franklahm@googlemail.com>
2011-05-04 17:07:41 +0200

    test506

--- test/T2_Dircache_attack.c
+++ test/T2_Dircache_attack.c
@@ -31,7 +31,7 @@
       mv dir1 dir2/renamed
 
    client 1:
-      stat dir2/renamed
+      stat renamed
 
    Check: CNID must not change
    Targets: getfildirparms()
@@ -82,7 +82,7 @@
       mv dir1 renamed1
 
    client 1:
-      stat renamed1/dir2/file
+      stat file
 
    Check: CNID must not change
    Targets: getfildirparms()
@@ -98,13 +98,27 @@
       mv dir1 renamed1
 
    client 1:
-      stat renamed1/dir2
+      stat dir2
 
    Check: CNID must not change
    Targets: getfildirparms()
 
- */
 
+   test506()
+   =========
+
+   client 1:
+      mkdir -p dir1/dir2
+
+   client 2:
+      mv dir1 renamed1
+      mkdir dir1
+
+   client 1:
+      stat dir2
+
+
+ */
 
 /* move and rename dir, enumerate new parent, stat renamed dir */
 STATIC void test500()
@@ -492,7 +506,7 @@ STATIC void test505()
 	}
 	if (strcmp(filedir.lname, subdir2
 )) {
-		fprintf(stderr,"\tFAILED %s should be %s\n",filedir.lname, renamedsubdir1);
+		fprintf(stderr,"\tFAILED %s should be %s\n",filedir.lname, subdir2);
         failed();
 	}
 
@@ -507,6 +521,76 @@ test_exit:
 	exit_test("test505");
 }
 
+/* stat subdir in poisened path */
+STATIC void test506()
+{
+    DSI *dsi = &Conn->dsi;
+    uint16_t vol1 = VolID;
+    uint16_t vol2;
+    char *dir = "t506 dir";
+    char *subdir1 = "t506 subdir1";
+    char *subdir2 = "t506 subdir2";
+    char *renamedsubdir1 = "t506 renamedsubdir1";
+    uint32_t dir_id, subdir1_id, subdir2_id, poisondir_id;
+    int ofs = 3 * sizeof(uint16_t);
+    struct afp_filedir_parms filedir;
+    uint16_t bitmap = (1<< DIRPBIT_DID)|(1<< DIRPBIT_LNAME);
+
+	enter_test();
+    fprintf(stderr, "===================\n");
+    fprintf(stderr, "Dircache:test506: stat subdir in poisened path\n");
+
+	if (!Conn2) {
+		test_skipped(T_CONN2);
+		goto test_exit;
+	}		
+	if ((vol2 = FPOpenVol(Conn2, Vol)) == 0xffff) {
+        failed();
+        goto test_exit;
+    }
+
+    /* Create directories with first connection */
+    if ((dir_id = FPCreateDir(Conn, vol1, DIRDID_ROOT, dir)) == 0)
+        failed();
+    if ((subdir1_id = FPCreateDir(Conn, vol1, dir_id, subdir1)) == 0)
+        failed();
+    if ((subdir2_id = FPCreateDir(Conn, vol1, subdir1_id, subdir2)) == 0)
+        failed();
+
+    /* Move and rename dir with second connection */
+    FAIL( FPMoveAndRename(Conn2, vol2, dir_id, dir_id, subdir1, renamedsubdir1) );
+    /* Re-create renamed directory */
+    if ((poisondir_id = FPCreateDir(Conn2, vol2, dir_id, subdir1)) == 0)
+        failed();
+
+    /* Manually check name and CNID */
+	FAIL( FPGetFileDirParams(Conn, vol1, subdir2_id, "", 0, bitmap) );
+
+	filedir.isdir = 1;
+	afp_filedir_unpack(&filedir, dsi->data + ofs, 0, bitmap);
+
+	if (filedir.did != subdir2_id) {
+		fprintf(stderr,"\tFAILED %x should be %x\n",filedir.did, subdir2_id);
+        failed();
+	}
+	if (strcmp(filedir.lname, subdir2
+)) {
+		fprintf(stderr,"\tFAILED %s should be %s\n",filedir.lname, subdir2);
+        failed();
+	}
+
+
+fin:
+	FAIL( FPCloseVol(Conn2, vol2) );
+    FAIL( FPDelete(Conn, vol1, subdir2_id, "") );
+    FAIL( FPDelete(Conn, vol1, subdir1_id, "") );
+    FAIL( FPDelete(Conn, vol1, poisondir_id, "") );
+    FAIL( FPDelete(Conn, vol1, dir_id, "") );
+
+test_exit:
+	exit_test("test506");
+}
+
 void Dircache_attack_test()
 {
     fprintf(stderr, "===================\n");
@@ -517,4 +601,5 @@ void Dircache_attack_test()
     test503();
     test504();
     test505();
+    test506();
 }
