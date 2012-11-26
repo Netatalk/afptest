a3ade9a5e8c694ad68a0901c68ff6e1ba563ef67
Ralph Boehme <sloowfranklin@gmail.com>
2012-11-26 19:02:29 +0100

    fix logging

--- test/spectest.sh
+++ test/spectest.sh
@@ -80,32 +80,34 @@ if [ ! -z "$EA_VOLUME" -a ! -z "$EA_LOCALVOLPATH" ] ; then
     echo "Running tests with adouble:ea"
     echo "============================="
 
+    rm -f spectest-ea.log
+
 ##
     printf "Running spectest with one user ..."
-    ./spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest.log 2>&1
+    ./spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest-ea.log 2>&1
     check_return
 
 ##
     printf "Running spectest with two user ..."
-    ./spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -d "$USER2" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest.log 2>&1
+    ./spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -d "$USER2" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest-ea.log 2>&1
     check_return
 
 ##
     printf "Running spectest with local filesystem modifications..."
-    ./T2_spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -d "$USER2" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest.log 2>&1
+    ./T2_spectest -"$AFPVERSION" -h "$AFPSERVER" -p "$AFPPORT" -u "$USER1" -d "$USER2" -w "$PASSWD" -s "$EA_VOLUME" -c "$EA_LOCALVOLPATH" >> spectest-ea.log 2>&1
     check_return
 
     if [ $ret -ne 0 ] ; then
         echo The following individual tests failed
         echo =====================================
-        grep "summary.*FAIL" spectest.log | sed s/test//g | sort -n | uniq
+        grep "summary.*FAIL" spectest-ea.log | sed s/test//g | sort -n | uniq
         echo =====================================
     fi
 
     echo
     echo The following tests were skipped
     echo =====================================
-    grep "NOT TESTED" spectest.log | sed s/test//g | sort -n | uniq
+    grep "NOT TESTED" spectest-ea.log | sed s/test//g | sort -n | uniq
     echo =====================================
 
 fi
