66b5b77991679fa8b0b910d159f8592c5414b2b6
Ralph Boehme <slow@samba.org>
2016-10-05 10:53:27 +0200

    Add test that checks FPGetExtAttr req_count
    Signed-off-by: Ralph Boehme <slow@samba.org>

--- test/FPGetACL.c
+++ test/FPGetACL.c
@@ -99,6 +99,63 @@ test_exit:
 	exit_test("test399");
 }
 
+STATIC void test432()
+{
+	u_int16_t vol = VolID;
+	u_int16_t bitmap;
+	DSI *dsi;
+	char *file="test432_file";
+	char *attr_name="test432_attribute";
+	int ret;
+
+	dsi = &Conn->dsi;
+
+	enter_test();
+	fprintf(stdout,"===================\n");
+	fprintf(stdout,"FPGetExtAttr:test399: check Extended Attributes Support\n");
+	if (Conn->afp_version < 30) {
+		test_skipped(T_AFP3);
+		goto test_exit;
+	}
+
+	if ( !(get_vol_attrib(vol) & VOLPBIT_ATTR_EXTATTRS)) {
+		test_skipped(T_UTF8);
+		goto test_exit;
+	}
+
+	bitmap = (1<< FILPBIT_PDID) | (1<<FILPBIT_LNAME) | (1<<FILPBIT_FNUM ) | (1<<FILPBIT_RFLEN);
+
+	if (FPCreateFile(Conn, vol,  0, DIRDID_ROOT , file)) {
+		nottested();
+		goto test_exit;
+	}
+
+	FAIL(FPSetExtAttr(Conn,vol, DIRDID_ROOT, 2, file, attr_name, "1234567890"))
+
+	/*
+	 * The xattr we set is 10 bytes large. The req_count must be
+	 * at least 10 bytes + 6 bytes. The 6 bytes are the AFP reply
+	 * packets bitmap and length field.
+	 *
+	 * This means that Apple's protocol implementation doesn't
+	 * support returning truncated xattrs. From the spec it would
+	 * be possible to get just the first byte of an xattr by
+	 * specifying an req_count of 7.
+	 */
+	EXPECT_FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 1, file, attr_name), AFPERR_PARAM );
+	EXPECT_FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 6, file, attr_name), AFPERR_PARAM );
+	EXPECT_FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 7, file, attr_name), AFPERR_PARAM );
+	EXPECT_FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 15, file, attr_name), AFPERR_PARAM );
+
+	FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 16, file, attr_name) );
+	FAIL( FPGetExtAttr(Conn,vol, DIRDID_ROOT , 0, 17, file, attr_name) );
+
+	FPDelete(Conn, vol,  DIRDID_ROOT , file);
+
+test_exit:
+	exit_test("test432");
+}
+
 /* ----------- */
 void FPGetACL_test()
 {
@@ -107,5 +164,6 @@ void FPGetACL_test()
 
     test398();
     test399();
+    test432();
 }
 
